89ef8783c6655f0e3e82e74ad18734370dbddef3
Jarifa <39035187+Jarifa@users.noreply.github.com>
2018-05-06 23:38:22 +0200

    Testbot.py

--- Testbot.py
+++ Testbot.py
@@ -7,6 +7,8 @@ import asyncio
 
 bot = discord.Client()
 bot = commands.Bot(command_prefix='#')
+botTitle = "Testbot"
+botVersion = "1.0"
 
 @bot.event
 async def on_ready():
@@ -24,5 +26,11 @@ async def on_message(message):
         content = message.content[6:]
         cubified = "placeholder"
         await bot.send_message(message.channel, "```"+cubified+"```")
+
+    if message.content.startswith('#help'):
+        embed = discord.Embed(title=botTitle+" version: "+botVersion, description="The list of commands are:")
+        embed.add_field(name="#ping", value="returns 'Pong!'")
+        embed.add_field(name="#cube", value="returns cubified version of input")
+
 
 bot.run("NDQyNzg1NTg2NDAzNjA2NTU5.DdD7SA.7vDn92RKl0pVOczdCNrcelD2mD0")
