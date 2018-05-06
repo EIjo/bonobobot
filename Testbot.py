68dcba072b39d826ad24dc3a103431907c880e56
Jarifa <davevdschouw@gmail.com>
2018-05-07 01:46:18 +0200

    Formatting and Token (still not working)

--- Testbot.py
+++ Testbot.py
@@ -1,12 +1,10 @@
 # Testbot by EIjo & Jarifa
 
 import discord
-import cubify
 from discord.ext import commands
-from discord.ext.commands import Bot
-import asyncio
 
-bot = discord.Client()
+import cubify
+
 bot = commands.Bot(command_prefix='#')
 botTitle = "Testbot"
 botVersion = "1.0"
@@ -14,23 +12,27 @@ botVersion = "1.0"
 
 @bot.event
 async def on_ready():
-    print("moker")
-    print("Bot Version: "+botTitle+" "+botVersion)
+    print("Bot starting...")
+    print("Bot Version: " + botTitle + " " + botVersion)
     print("userid: " + bot.user.id)
     print("Startup complete.")
+
 
 @bot.event
 async def on_message(message):
     if message.content.startswith('#ping'):
-        userID = message.author.id
-        await bot.send_message(message.channel, "<@%s> Pong!" % (userID))
+        user_id = message.author.id
+        await bot.send_message(message.channel, "<@%s> Pong!" % (user_id))
 
     if message.content.startswith('#cube'):
         content = message.content[6:]
         cubified = cubify.cubify(word=content, subsqaures=(0 == 0))
         if not cubified:
-            await bot.send_message(message.channel, "invalid text try a world with at least 5 letters and with the same letter at the beginning as the end")
-        else :
+            await bot.send_message(message.channel,
+                                   "Invalid text. \n"
+                                   "try a world that starts and ends with the same letter\n"
+                                   "The word also has to be at least 5 letters long")
+        else:
             await bot.send_message(message.channel, "```\n" + cubified + "```")
 
     if message.content.startswith('#help'):
@@ -41,45 +43,8 @@ async def on_message(message):
         await bot.send_message(message.channel, embed=embed)
 
 
-bot.run(input("Input bot token: "))
-# Testbot by EIjo & Jarifa
-
-import discord
-import cubify
-from discord.ext import commands
-from discord.ext.commands import Bot
-import asyncio
-
-bot = discord.Client()
-bot = commands.Bot(command_prefix='#')
-botTitle = "Testbot"
-botVersion = "1.0"
-
-
-@bot.event
-async def on_ready():
-    print("Bot starting...")
-    print("Bot Version: "+botTitle+" "+botVersion)
-    print("userid: " + bot.user.id)
-    print("Startup complete.")
-
-@bot.event
-async def on_message(message):
-    if message.content.startswith('#ping'):
-        userID = message.author.id
-        await bot.send_message(message.channel, "<@%s> Pong!" % (userID))
-
-    if message.content.startswith('#cube'):
-        content = message.content[6:]
-        cubified = cubify.cubify(word=content, subsqaures=(0 == 0))
-        await bot.send_message(message.channel, "```\n" + cubified + "```")
-
-    if message.content.startswith('#help'):
-        embed = discord.Embed(title=botTitle + " version: " + botVersion, description="The list of commands are:")
-        embed.add_field(name="#ping", value="returns 'Pong!'")
-        embed.add_field(name="#cube", value="returns cubified version of input")
-
-        await bot.send_message(message.channel, embed=embed)
-
-
-bot.run(input("Input bot token: "))
+try:
+    with open("testfile.txt") as f:
+        bot.run(f.read())
+except Exception:
+    print("Invalid token")
